0fd37ba76937167bc97791a36cce06a45f506496
winsoheb <winsoheb@gmail.com>
2026-04-02 00:38:52 +0530

    Fix blank screen by bringing in missing Link import from react-router-dom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import FeaturedServices from '../components/FeaturedServices';
 import TestimonialsCarousel from '../components/TestimonialsCarousel';
 
